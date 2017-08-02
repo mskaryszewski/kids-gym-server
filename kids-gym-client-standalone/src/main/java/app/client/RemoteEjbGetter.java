8f54731c09511b41bdcc1b177fd5c3d1c24ff65b
Michal Skaryszewski <michal.skaryszewski@gmail.com>
2017-08-03 01:15:47 +0200

    ClientApp, small code cleansing

---
+++ kids-gym-client-standalone/src/main/java/app/client/RemoteEjbGetter.java
@@ -0,0 +1,54 @@
+package app.client;
+
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ * This class retrieves EJBs by calculating given interfaceName class by removing packageName.Remote prefix and adding Bean suffix.
+ * Example:
+ *   InterfaceName: this.is.my.package.RemoteCalculator
+ *   calculated BeanName: CalculatorBean
+ * 
+ * For all beans/interfaces that do not follow this convention, remote EJB will not be retrieved correctly.
+ * 
+ * @author Michal
+ *
+ */
+public enum RemoteEjbGetter {
+	
+	INSTANCE;
+	
+	private RemoteEjbGetter() {
+		Properties properties = new Properties();
+		properties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+		try {
+			context = new InitialContext(properties);
+		} catch (NamingException e) {
+			System.out.println("Context was not built correctly " + e.getMessage());
+		}
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public <U> U getRemoteEjbAndCast(Class clazz) throws NamingException {
+		final String interfaceName = clazz.getName();
+		String lookupName = generateLookupName(interfaceName);
+		return (U) context.lookup(lookupName);
+	}
+	
+	private String generateLookupName (String interfaceName) {
+		final String beanName   = getBeanName(interfaceName);
+		return "ejb:" + APP_NAME + "/" + EJB_MODULE_NAME + "/" + beanName + "!" + interfaceName;
+	}
+	
+	private String getBeanName(String interfaceName) {
+		return interfaceName.replaceFirst(".*Remote", "").concat("Bean");
+	}
+	
+	private Context context;
+
+	private static final String APP_NAME        = "kids-gym-app-0.0.1-SNAPSHOT";
+	private static final String EJB_MODULE_NAME = "kids-gym-ejb-0.0.1-SNAPSHOT";
+}
